5665cbd68a0894e72dff30386f3da7af36f835ae
Eli Lu <eli@IQAS.htc.com>
2017-04-10 15:37:39 +0800

    Move founctions at example/main.go to lib/jira.go

--- example/main.go
+++ example/main.go
@@ -1,114 +1,15 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 	"os"
 	"strings"
 
 	"github.com/luyaotsung/jiraGetIssues/lib"
 )
 
-// this feature can get labes merge strings
-func getLabels(d []string) string {
-	name := ""
-	for z := range d {
-		name = name + "," + d[z]
-	}
-	if name == "" {
-		return ""
-	}
-	return name[1:len(name)]
-}
-
-// this feature can get fix versions merge strings
-func getFixVersions(d []jira.JiraFieldsFixVersions) string {
-	name := ""
-	for z := range d {
-		name = name + "," + d[z].Name
-	}
-	if name == "" {
-		return ""
-	}
-	return name[1:len(name)]
-}
-
-// this feature can get versions merge strings
-func getVersions(d []jira.JiraFieldsVersions) string {
-	name := ""
-	for z := range d {
-		name = name + "," + d[z].Name
-	}
-	if name == "" {
-		return ""
-	}
-	return name[1:len(name)]
-}
-
-// this feature can get component merge strings
-func getComponents(d []jira.JiraFieldsResolution) string {
-	name := ""
-	for z := range d {
-		name = name + "," + d[z].Name
-	}
-	if name == "" {
-		return ""
-	}
-	return name[1:len(name)]
-}
-
-func getReturnJSON(webaddress string, username string, password string, startcount int, totalcount int) jira.JiraObject {
-
-	client := &http.Client{}
-
-	// Number is from 0 maximum is 1000 at one times.
-	getProjectIssueList := fmt.Sprintf("%s/rest/api/2/search?jql=project+in+(+GMM+,+TES+)+order+by+id&startAt=%d&maxResults=%d", webaddress, startcount, totalcount)
-	req, err := http.NewRequest("GET", getProjectIssueList, nil)
-	req.SetBasicAuth(username, password)
-	req.Header.Add("Content-Type", "application/json")
-	resp, err := client.Do(req)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Clone http response
-	var bodyBytes []byte
-	if resp.Body != nil {
-		bodyBytes, _ = ioutil.ReadAll(resp.Body)
-	}
-	respError := ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
-	resp.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
-
-	xSeraphLoginReason := resp.Header.Get("X-Seraph-LoginReason")
-
-	fmt.Println(" Login Feedback is ==>", xSeraphLoginReason)
-
-	var jiraObject jira.JiraObject
-	errorMessage := json.NewDecoder(resp.Body).Decode(&jiraObject)
-	if errorMessage != nil {
-		log.Fatal(errorMessage)
-	}
-
-	// If value of Total it seems Project name is error.
-	if jiraObject.Total == 0 {
-		var errorMessageObject jira.ObjectErrorMessage
-		json.NewDecoder(respError).Decode(&errorMessageObject)
-		for x := range errorMessageObject.ErrorMessages {
-			fmt.Println(errorMessageObject.ErrorMessages[x])
-		}
-		log.Fatal("Issue of Project is empty")
-	}
-	return jiraObject
-}
-
 func main() {
-
 	jiraweb := flag.String("jiraweb", "", "Jira Web Site Address, example : -jiraweb=https://inhouse.htcstudio.com/jira")
 	username := flag.String("username", "", "User Name")
 	password := flag.String("password", "", "Password")
@@ -118,7 +19,7 @@ func main() {
 	if flag.NFlag() == 3 {
 
 		// func getReturnJSON( webaddress string, username string, password string, startcount int, totalcount int) jira.JiraObject {
-		jiraObject := getReturnJSON(*jiraweb, *username, *password, 0, 1)
+		jiraObject := jira.GetReturnJSON(*jiraweb, *username, *password, 0, 1)
 
 		fmt.Println("Total : ", jiraObject.Total)
 		fmt.Println("MaxResult : ", jiraObject.MaxResults)
@@ -128,22 +29,20 @@ func main() {
 		fmt.Println("Range Count => ", rangeCount)
 
 		for i := 0; i < rangeCount; i++ {
-
 			startCount := 1000 * i
 			totalCount := 1
-
 			fmt.Println("Strat Count --> ", startCount)
 
 			// func getReturnJSON( webaddress string, username string, password string, startcount int, totalcount int) jira.JiraObject {
-			jiraObject = getReturnJSON(*jiraweb, *username, *password, startCount, totalCount)
+			jiraObject = jira.GetReturnJSON(*jiraweb, *username, *password, startCount, totalCount)
 
 			for x := range jiraObject.Issues {
 				Key := jiraObject.Issues[x].Key
 
 				fmt.Println("Key:", Key, "Issue TYPE:", jiraObject.Issues[x].Fields.IssueType.Name)
 
-				componentMerged := getComponents(jiraObject.Issues[x].Fields.Components)
-				labelsMerged := getLabels(jiraObject.Issues[x].Fields.Labels)
+				componentMerged := jira.GetComponents(jiraObject.Issues[x].Fields.Components)
+				labelsMerged := jira.GetLabels(jiraObject.Issues[x].Fields.Labels)
 
 				fmt.Println("Components -->", componentMerged, "Labels -->", labelsMerged)
 			}
--- lib/jira.go
+++ lib/jira.go
@@ -1,5 +1,14 @@
 package jira
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 // JiraObject is the first layer of jql search
 type JiraObject struct { //xxxx
 	Expend     string       `json:"expend"`
@@ -171,7 +180,95 @@ type ObjectErrorMessage struct {
 	Errors        interface{} `json:"errors"`
 }
 
-// GetMergetName is the get merge name struct used
-type GetMergeName struct {
-	Name string `json:"name"`
+// this feature can get labes merge strings
+func GetLabels(d []string) string {
+	name := ""
+	for z := range d {
+		name = name + "," + d[z]
+	}
+	if name == "" {
+		return ""
+	}
+	return name[1:len(name)]
+}
+
+// this feature can get fix versions merge strings
+func GetFixVersions(d []JiraFieldsFixVersions) string {
+	name := ""
+	for z := range d {
+		name = name + "," + d[z].Name
+	}
+	if name == "" {
+		return ""
+	}
+	return name[1:len(name)]
+}
+
+// this feature can get versions merge strings
+func GetVersions(d []JiraFieldsVersions) string {
+	name := ""
+	for z := range d {
+		name = name + "," + d[z].Name
+	}
+	if name == "" {
+		return ""
+	}
+	return name[1:len(name)]
+}
+
+// this feature can get component merge strings
+func GetComponents(d []JiraFieldsResolution) string {
+	name := ""
+	for z := range d {
+		name = name + "," + d[z].Name
+	}
+	if name == "" {
+		return ""
+	}
+	return name[1:len(name)]
+}
+
+func GetReturnJSON(webaddress string, username string, password string, startcount int, totalcount int) JiraObject {
+
+	client := &http.Client{}
+
+	// Number is from 0 maximum is 1000 at one times.
+	getProjectIssueList := fmt.Sprintf("%s/rest/api/2/search?jql=project+in+(+GMM+,+TES+)+order+by+id&startAt=%d&maxResults=%d", webaddress, startcount, totalcount)
+	req, err := http.NewRequest("GET", getProjectIssueList, nil)
+	req.SetBasicAuth(username, password)
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Clone http response
+	var bodyBytes []byte
+	if resp.Body != nil {
+		bodyBytes, _ = ioutil.ReadAll(resp.Body)
+	}
+	respError := ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+	resp.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+
+	xSeraphLoginReason := resp.Header.Get("X-Seraph-LoginReason")
+
+	fmt.Println(" Login Feedback is ==>", xSeraphLoginReason)
+
+	var jiraObject JiraObject
+	errorMessage := json.NewDecoder(resp.Body).Decode(&jiraObject)
+	if errorMessage != nil {
+		log.Fatal(errorMessage)
+	}
+
+	// If value of Total it seems Project name is error.
+	if jiraObject.Total == 0 {
+		var errorMessageObject ObjectErrorMessage
+		json.NewDecoder(respError).Decode(&errorMessageObject)
+		for x := range errorMessageObject.ErrorMessages {
+			fmt.Println(errorMessageObject.ErrorMessages[x])
+		}
+		log.Fatal("Issue of Project is empty")
+	}
+	return jiraObject
 }
